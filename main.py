b5aad1440f2cd491d90db2527698082aa3146e8f
Wiikay <vinodkonkal.vk@gmail.com>
2025-04-24 17:21:34 +0000

    Refactor main.py to serve index.html and update requirements.txt to specify Flask version 3.0.0; add index.html with initial content for Car Connect application.

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import os
+from flask import Flask, render_template, send_from_directory
 
-from flask import Flask
-
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
-def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
-
+def index():
+    """Serve the index.html file."""
+    return send_from_directory('.', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
